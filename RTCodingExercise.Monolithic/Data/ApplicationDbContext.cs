76fdbb9f85d2dae32866d7a67638ded30077cbd3
Account 2 <Account 2@DESKTOP-9PCS4TA>
2024-05-28 02:51:10 +0100

    RTCoding Exercise - Monolithic

--- RTCodingExercise.Monolithic/Data/ApplicationDbContext.cs
+++ RTCodingExercise.Monolithic/Data/ApplicationDbContext.cs
@@ -10,5 +10,17 @@ namespace RTCodingExercise.Monolithic.Data
         }
 
         public DbSet<Plate> Plates { get; set; }
+
+        public List<Plate> PlatesWithSalesMarkup(List<Plate> plates)
+        {
+            var discount = 20; // TODO Store sales markup in the database
+
+            foreach (var plate in plates)
+            {
+                plate.SalePrice += Math.Round((plate.SalePrice / 100) * discount, 2);
+            }
+
+            return plates.ToList();
+        }
     }
 }
---
+++ RTCodingExercise.Monolithic/Domain/AddPlateViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace RTCodingExercise.Monolithic.Domain
+{
+    public class AddPlateViewModel
+    {
+        public Guid? Id { get; set; }
+
+        public string? Registration { get; set; }
+
+        public decimal PurchasePrice { get; set; }
+
+        public decimal SalePrice { get; set; }
+
+        public string? Letters { get; set; }
+
+        public int Numbers { get; set; }
+    }
+}
